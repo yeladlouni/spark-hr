c9664f2776284707064778c4ca71fd5b6d685ce1
= <=>
2020-12-09 09:57:02 +0100

    aggrégations

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,11 +1,15 @@
 import org.apache.spark.sql.SparkSession
 
-object SparkEmployees extends App {
+object Test extends App {
+
   val spark = SparkSession
     .builder()
     .appName("SparkHR")
     .config("spark.master", "local")
     .getOrCreate()
+
+  // Pour changer le niveau de journalisation/log
+  spark.sparkContext.setLogLevel("ERROR")
 
   val df = spark.read.format("csv")
   .option("header", "true")
@@ -15,14 +19,12 @@ object SparkEmployees extends App {
 
   df.printSchema()
 
-  df.show()
+  // Aggrégations pour chaque niveau de groupement
+  df.groupBy("DEPARTMENT_ID", "JOB_ID").sum("SALARY").show()
 
-  df.createOrReplaceTempView("V_EMPLOYEES")
+  // Aggrégations et sous-totaux "subtotals" pour chaque niveau
+  df.cube("DEPARTMENT_ID", "JOB_ID").sum("SALARY").show()
 
-  val df2 = spark.sql("SELECT COUNT(*) FROM V_EMPLOYEES");
-  df2.show()
-
-  df.columns.foreach(println)
-  df.select("LAST_NAME").show()
-  df.select("SALARY").summary().show()
+  // Aggrégations et sous-totaux pour le premier niveau
+  df.rollup("DEPARTMENT_ID", "JOB_ID").sum("SALARY").show()
 }
